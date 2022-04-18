a489940e2ee9c2675e825b0bc98c6db63cf549dd
Mashiur88 <39610424+Mashiur88@users.noreply.github.com>
2022-04-18 11:59:48 +0600

    Add files via upload

---
+++ Mashiur/sidebar.php
@@ -0,0 +1,14 @@
+<div class="sidebar">
+    <p><b>Sidebar</b></p>
+    <ul>
+        <li><a href="Webpage.php">Home</a></li>
+        <li><a href="gallery.php">Gallery</a></li>
+        <li><a href="contact.php">Contact</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="registration.php">Registration</a></li>
+        <li><a href=login.php">Login</a></li>
+        
+    </ul>
+</div>
+
+
